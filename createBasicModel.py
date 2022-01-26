49a58677b7bdfb299f080fc53b3f101c98276158
Amit Yahalom <yahalom1234amit@gmail.com>
2022-01-26 16:25:44 +0200

    Added functionality to import mnist dataset, create and train basic two layer model and test it on imported images

---
+++ createBasicModel.py
@@ -0,0 +1,58 @@
+import cv2
+import tensorflow as tf
+import numpy as np
+
+
+def load_mnist():
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    return (x_train, y_train), (x_test, y_test)
+
+
+def create_model(num_layers, neurons_per_layer, mnist_shape):
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(mnist_shape, mnist_shape)))
+    for i in range(0, num_layers):
+        model.add(tf.keras.layers.Dense(
+            units=neurons_per_layer, activation='relu'))
+    model.add(tf.keras.layers.Dense(
+        units=neurons_per_layer, activation='softmax'))
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+def train_model(x_train=None, y_train=None, num_layers=2, neurons_per_layer=128, mnist_shape=28, Epochs=5):
+    #     if x_train is None or y_train is None:
+    #     (x_train, y_train), (x_test, y_test) = load_mnist()
+    # if model is None:
+    #     model = create_model(num_layers, neurons_per_layer, mnist_shape)
+    (x_train, y_train), (x_test, y_test) = load_mnist()
+    model = create_model(num_layers, neurons_per_layer, mnist_shape)
+    model.fit(x_train, y_train, epochs=Epochs)
+    return model, (x_train, y_train), (x_test, y_test)
+
+
+def model_loss_and_accuracy(model, x_test, y_test):
+    loss, accuracy = model.evaluate(x_test, y_test)
+    return loss, accuracy
+
+
+def test_with_image(model, img):
+    img = cv2.imread(img)[:, :, 0]
+    img = np.invert(np.array([img]))
+    prediction = model.predict(img)
+    print(np.argmax(prediction))
+
+
+def main():
+    # model = None
+    model, (x_train, y_train), (x_test, y_test) = train_model()
+    loss, accuracy = model_loss_and_accuracy(model, x_test, y_test)
+    print(
+        f'loss of the model is {loss}, and accuracy of the model is {accuracy}')
+
+    test_with_image(model, "test_two_digit.png")
+
+
+if __name__ == "__main__":
+    main()
